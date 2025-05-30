d54edbb6245bbf208b89819de1a9a305d9f62011
cezar-cramariuc <37026039+cezar-cramariuc@users.noreply.github.com>
2025-05-30 21:00:10 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ro">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Economia interbelică/title>
+        <link href="stil/stil.css" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+</html>
