dacd6acc11d08b3596cf8fde98d5be0dd79397ee
cezar-cramariuc <37026039+cezar-cramariuc@users.noreply.github.com>
2025-06-02 18:51:51 +0300

    qq

---
+++ module/acasa.php
@@ -0,0 +1,9 @@
+<section>
+    <div id="subiecte">
+        <h1>Subiecte</h1>
+    </div>
+    <ul>
+        <li><a href="index.php?p=acasa" id="acasa">Acasă</a></li>
+        <li><a href="index.php?p=bataliaGranelor">Bătălia grânelor</a></li>
+    </ul>
+</section>
