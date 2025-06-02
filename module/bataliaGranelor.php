dd122930c48c7b54640c5d546e03e530a861c8b8
cezar-cramariuc <37026039+cezar-cramariuc@users.noreply.github.com>
2025-06-02 17:21:19 +0300

    aaa

---
+++ module/bataliaGranelor.php
@@ -0,0 +1,34 @@
+<section>
+    <h1>Pe frontul cerealelor, 1928 Pe frontul cerealelor</h1>
+    Data și locul
+    1928, URSS
+    Sursa
+    primară – discurs politic Autorul
+    Joseph Vissarionovich Stalin (1878–1953)
+    <h1>Descriere</h1>
+    <p>Stalin a fost liderul politic al Uniunii Sovietice din 1924 până la moartea sa în 1953. Această sursă este un
+        fragment dintr-un discurs pe care l-a ținut ca răspuns la o întrebare a unui student cu privire la problemele cu aprovizionarea cu cereale. S-a întâmplat în timpul unei discuții la Institutul Profesorilor Roșii, Academia
+        Comunistă și Universitatea Sverdlov pe 28 mai 1928. Primul plan cincinal a început la scurt timp după această
+        discuție din octombrie 1928. O parte a planului a fost numită "colectivizare", care a forțat fermele private să fie
+        combinate în ferme colective de stat. Ca răspuns, mulți proprietari de ferme (culaci) și-au distrus culturile și
+        animalele, ceea ce a dus la o lipsă de alimente. Aceste penurii s-au înrăutățit, deoarece alimentele disponibile
+        nu au fost distribuite bine.</p>
+
+    <h2>Fragment Joseph Vissarionovich Stalin (1878–1953) - discurs</h2>
+    <figure>
+        <blockquote>
+            <p>Cauza principală a dificultăților noastre cu cerealele este că creșterea producției de cereale pentru piață nu ține pasul cu creșterea cererii de cereale. Industria este în creștere. Numărul lucrătorilor este în creștere. Orașele sunt în creștere. Și, în sfârșit, regiunile producătoare de culturi industriale (bumbac, in, sfeclă de zahăr etc.) sunt în creștere, creând o cerere de cereale. Toate acestea duc la o creștere rapidă a cerințelor noastre în ceea ce privește cerealele disponibile pentru piață. Dar producția de cereale pentru piață crește într-un ritm dezastruos de lent. …
+                Elementele capitaliste din districtele rurale, și în primul rând culacii, au profitat de aceste dificultăți pentru a perturba politica economică sovietică. …
+                Care este calea de ieșire din situație?
+                Unii oameni văd calea de ieșire din situație într-o întoarcere la agricultura kulacă, în dezvoltarea și extinderea agriculturii culaki. Acești oameni nu îndrăznesc să pledeze pentru o întoarcere la agricultura proprietarilor, pentru că își dau seama, evident, că astfel de discuții sunt periculoase în vremurile noastre. … Acești oameni cred că puterea sovietică se poate baza simultan pe două clase opuse – clasa culacilor, al cărei principiu economic este exploatarea clasei muncitoare, și clasa muncitorilor, al cărei principiu economic este abolirea oricărei exploatări. …</p>
+            <ol>
+                <li>Ieșirea constă, în primul rând, în trecerea de la fermele țărănești mici, înapoiate și împrăștiate la ferme comune amalgamate, la scară largă, echipate cu mașini, înarmate cu cunoștințe științifice și capabile să producă un maxim de cereale pentru piață. Soluția constă în trecerea de la agricultura țărănească individuală la agricultura colectivă, la agricultura comună. …</li>
+                <li>Ieșirea constă, în al doilea rând, în extinderea și consolidarea vechilor ferme de ardezie și în organizarea și dezvoltarea de noi ferme mari de stat. …</li>
+                <li>În cele din urmă, calea de ieșire constă în creșterea sistematică a randamentului fermelor țărănești individuale mici și mijlocii. Nu putem și nu ar trebui să acordăm niciun sprijin fermelor mari de kulak individuale. Dar putem și trebuie să sprijinim fermele mici și mijlocii, ajutându-le să-și crească randamentul culturilor și atrăgându-le în canalul de organizare cooperativă.</li>
+            </ol>
+            <figcaption>
+                <cite> Colecția Stalin, Joseph și Fry de istorie și cultură italiană. Probleme ale leninismului. New York: International Publishers, 1934.</cite>
+        </blockquote>
+        </figcaption>
+    </figure>
+</section>
